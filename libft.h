b676606b44e7dace82153a2fac1e7ea98553d45d
mmch <abicer.pro@gmail.com>
2021-05-06 03:00:45 +0200

    norme v3

--- libft.h
+++ libft.h
@@ -66,7 +66,7 @@ void			ft_putnbr_fd(int n, int fd);
 **	Bonus
 */
 
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
@@ -80,8 +80,8 @@ void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap
-				(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(
+					t_list *s, void *(*f)(void *), void (*del)(void *));
 
 /*
 ** P. Perso - LIBC
